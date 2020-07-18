ed395817c432462b2698616a818f468febba12af
Zakir Rahimli <60932492+RahimliZakir@users.noreply.github.com>
2020-07-18 07:26:12 +0400

    Add files via upload

---
+++ C#-Parse-TryParse-sizeof-default-GetType()-typeof/Parse, TryParse/Parse, TryParse ConsoleApp/MathOperationsConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inta, intb;
+
+            Console.Write("1-ci elementi daxil edin");
+            string a = Console.ReadLine();
+            bool cavab1 = int.TryParse(a, out inta);
+            if (cavab1 == false)
+            {
+                Console.WriteLine("Zehmet olmasa reqem va yaxud eded daxil edin");
+            }
+            Console.Write("2-ci elementi daxil edin");
+            string b = Console.ReadLine();
+            bool cavab2 = int.TryParse(b, out intb);
+
+            if (cavab2 == false)
+            {
+                Console.WriteLine("Zehmet olmasa reqem va yaxud eded daxil edin");
+            }
+
+            
+
+            
+            int c = inta + intb;
+            Console.WriteLine($"{inta}+{intb}={c}");
+
+            Console.ReadKey();
+        }
+    }
+}
