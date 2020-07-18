499ea71e2e654835ae954c2422f2bc388ff82de4
Zakir Rahimli <60932492+RahimliZakir@users.noreply.github.com>
2020-07-18 07:35:16 +0400

    Add files via upload

---
+++ Method Callback (Bulit-In Delegates) - C#/CallbackSolution/CallbackConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbackConsoleApp
+    //Built-In Delegates (Method Callback)
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CallBack(EdedVurulsunBesh));
+            Console.ReadKey();
+        }
+
+        static int EdedVurulsunBesh(int eded1, int eded2)
+        {
+            return (eded1 + eded2) * 5;
+        }
+
+        //Əgər callback etdiyimiz method value return eləmirsə, Func<> yox, Action<> yazılır.
+        static int CallBack(Func<int,int,int>BirMethod)
+        {
+            return 10 + (BirMethod(12, 8));
+        }
+    }
+}
