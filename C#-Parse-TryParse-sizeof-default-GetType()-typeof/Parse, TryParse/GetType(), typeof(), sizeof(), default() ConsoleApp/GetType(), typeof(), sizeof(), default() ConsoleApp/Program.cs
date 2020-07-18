114db0bde6a79cdedb96865787d97260ae0fd61e
Zakir Rahimli <60932492+RahimliZakir@users.noreply.github.com>
2020-07-18 07:28:31 +0400

    Add files via upload

---
+++ C#-Parse-TryParse-sizeof-default-GetType()-typeof/Parse, TryParse/GetType(), typeof(), sizeof(), default() ConsoleApp/GetType(), typeof(), sizeof(), default() ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetType____typeof____sizeof____default___ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Type typeType = typeof(int);
+            Console.WriteLine(typeType);
+
+            Type elementType = true.GetType();
+            Console.WriteLine(elementType);
+
+            var typeSize = sizeof(char);
+            Console.WriteLine(typeSize);
+
+            var typeDefault = default(sbyte);
+            Console.WriteLine(typeDefault);
+
+            var Min = decimal.MinValue;
+            var Max = decimal.MaxValue;
+            Console.WriteLine($"{Min} ve {Max}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
